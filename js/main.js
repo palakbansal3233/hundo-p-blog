0e3e72bb34e0c4387d445c0652a1b1fe83a103a8
Palak Bansal <palakbansal3233@gmail.com>
2020-06-04 19:54:19 +0530

    Site-content

--- js/main.js
+++ js/main.js
@@ -8,6 +8,13 @@ $(document).ready(function(){
     })
     
     //carousel 
-    $('.owl-carousel').owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    });
 
 }) 
